4103380930e18d4d5914031be3db7e7a47c08c4e
Miguel_Avalos <miguel.avalos@utec.edu.pe>
2023-11-14 23:50:34 -0500

    te quiero mucho jhimy

--- AniList-Back-End/hilos_respuestas (modificar)/DEMO/src/main/java/com/example/demo/CapaSeguridad/config/SecurityConfiguration.java
+++ AniList-Back-End/hilos_respuestas (modificar)/DEMO/src/main/java/com/example/demo/CapaSeguridad/config/SecurityConfiguration.java
@@ -50,7 +50,9 @@ public class SecurityConfiguration {
     private RequestMatcher publicEndpoints() {
         return new OrRequestMatcher(
                 new AntPathRequestMatcher("/usuarios/**"),
-                new AntPathRequestMatcher("/api/**")
+                new AntPathRequestMatcher("/api/**"),
+                new AntPathRequestMatcher("/hilos/**"),
+                new AntPathRequestMatcher("/respuestas/**")
         );
     }
 
