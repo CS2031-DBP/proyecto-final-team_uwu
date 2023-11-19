da405aaa02b950d9ddd975230567e622ed7f4e22
Miguel_Avalos <miguel.avalos@utec.edu.pe>
2023-11-19 15:52:21 -0500

    commit

---
+++ AniList-Back-End/hilos_respuestas (modificar)/DEMO/src/main/java/com/example/demo/Estados/domain/EstadoRespository.java
@@ -0,0 +1,4 @@
+package com.example.demo.Estados.domain;
+
+public class EstadoRespository  {
+}
---
+++ AniList-Back-End/hilos_respuestas (modificar)/DEMO/src/main/java/com/example/demo/Estados/domain/Estados.java
@@ -0,0 +1,8 @@
+package com.example.demo.Estados.domain;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Estados {
+}
